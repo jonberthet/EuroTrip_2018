3baf40a86fa1efb068eec6accdf33f7a6a99538f
Jon Berthet <jberthet@uchicago.edu>
2018-06-01 12:43:33 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1l5VtLRYqDwGKXC76J_lnsaMc6A58HxuxF5_vDlMX0cA/edit#gid=0';
